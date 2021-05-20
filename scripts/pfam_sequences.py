2820ac579258ed2bf01d01e18885736c0a509ea2
Patrycja Owczarek <patrycja.k.owczarek@gmail.com>
2021-05-20 15:48:51 +0200

    add clan_seq function

--- scripts/pfam_sequences.py
+++ scripts/pfam_sequences.py
@@ -28,6 +28,38 @@ def family_seq(families, format='fasta', type='full', download = False, path = '
         if download:
             pathname = '%s/%s.%s' % (path, family, format)
             f = open(pathname, 'wb').write(r.content)
-        else:
-            raise ValueError("incorrect name of family: %s" % family)
+    return result
+
+
+def clan_seq(clans, format='fasta', type='full', download = False, path = '.'):
+    """TO-DO
+    """
+    result = []
+    formats = ['fasta', 'selex', 'stockholm', 'msf']
+    types = ['seed', 'full']
+    if format not in formats:
+        raise ValueError("format can be one of following: fasta, selex, stockholm, msf")
+    if type not in types:
+        raise ValueError("type can be one of following: seed, full")
+    if not isinstance(clans, (list, str)):
+        raise ValueError("incorrect type of families name")
+    if isinstance(clans, str):
+        clans = [clans]
+    for clan in clans:
+        clan_seq = ''
+        clan_url = 'https://pfam.xfam.org/clan/%s#tabview=tab2' % clan
+        r_clan = requests.get(clan_url, allow_redirects=True)
+        r_clan.raise_for_status()
+        families = re.findall('PF[0-9]{5}',r_clan.text)
+        for family in set(families):
+            url = 'https://pfam.xfam.org/family/%s' % family
+            url += '/alignment/%s' % type
+            url += '/format?format=%s&alnType=%s&order=a&case=l&gaps=dashes&download=0' % (format, format)
+            r = requests.get(url, allow_redirects=True)
+            r.raise_for_status()
+            clan_seq += r.text
+            result.append(r.text)
+        if download:
+            pathname = '%s/%s.%s' % (path, clan, format)
+            f = open(pathname, 'w').write(clan_seq)
     return result
