dd98e482652742a8471c6ba37c2b4dceff526940
Patrycja Owczarek <patrycja.k.owczarek@gmail.com>
2021-05-20 15:17:44 +0200

    family seq search based also on name

--- scripts/pfam_sequences.py
+++ scripts/pfam_sequences.py
@@ -19,20 +19,15 @@ def family_seq(families, format='fasta', type='full', download = False, path = '
     if isinstance(families, str):
         families = [families]
     for family in families:
-        print(family)
-        if bool(re.match("PF[0-9]{5}$", family)):
-            url = 'https://pfam.xfam.org/family/%s' % family
-            url += '/alignment/%s' % type
-            url += '/format?format=%s&alnType=%s&order=a&case=l&gaps=dashes&download=0' % (format, format)
-            r = requests.get(url, allow_redirects=True)
-            print(url)
-            r.raise_for_status()
-            result.append(r.text)
-            if download:
-                pathname = '%s/%s.%s' % (path, family, format)
-                f = open(pathname, 'wb').write(r.content)
+        url = 'https://pfam.xfam.org/family/%s' % family
+        url += '/alignment/%s' % type
+        url += '/format?format=%s&alnType=%s&order=a&case=l&gaps=dashes&download=0' % (format, format)
+        r = requests.get(url, allow_redirects=True)
+        r.raise_for_status()
+        result.append(r.text)
+        if download:
+            pathname = '%s/%s.%s' % (path, family, format)
+            f = open(pathname, 'wb').write(r.content)
         else:
             raise ValueError("incorrect name of family: %s" % family)
     return result
-
-
