f1ac4b2ea7ec21bf35283dc6c648dc07907b0edd
xadam97x <45838015+xadam97x@users.noreply.github.com>
2021-05-21 13:34:57 +0200

    Create pdb_to_pfam.py

---
+++ scripts/pdb_to_pfam.py
@@ -0,0 +1,21 @@
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+from uniprot_to_pfam import *
+
+
+def pdb_to_pfam(pdb_id_list):
+    data = {'pdb name': [], 'pfam familly': []}
+    table = pd.DataFrame(data)
+    for pdb_id in pdb_id_list:
+        url = 'https://www.rcsb.org/structure/{}'.format(pdb_id)
+        html = urlopen(url)
+        soup = BeautifulSoup(html, 'html.parser')
+        for a in soup.find_all('a', href=True):
+            if bool(re.match('https://www.uniprot.org/uniprot/[A-Z0-9]{6}', a['href'])):
+                uniprot_id = a['href'].replace("https://www.uniprot.org/uniprot/", "")
+                families = uniprot_to_pfam([uniprot_id])['pfam familly'].iloc[0]
+                new_row = {'pdb name': pdb_id, 'pfam familly': families}
+                table = table.append(new_row, ignore_index=True)
+    return table
