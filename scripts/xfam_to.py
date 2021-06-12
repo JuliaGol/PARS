556908fe3aa659512a58c5b5c3e6ba0677ba4618
xadam97x <45838015+xadam97x@users.noreply.github.com>
2021-06-12 17:11:05 +0200

    add docstrings

--- scripts/xfam_to.py
+++ scripts/xfam_to.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen, Request
 from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
 
-def xfam_to(url, pattern, sep='/'):
+def _xfam_to(url, pattern, sep='/'):
     try:
         html=urlopen(url)
     except (HTTPError, URLError) as e:
@@ -20,46 +20,55 @@ def xfam_to(url, pattern, sep='/'):
 
         
 def rfam_to_pubmed(family):
+    """get a list of associated pubmed ids for given rfam access key."""
     url ='https://rfam.xfam.org/family/'+family
     pattern='http://www.ncbi.nlm.nih.gov/pubmed/'
-    return ["".join(filter(str.isdigit, x)) for x in xfam_to(url, pattern)]
+    return ["".join(filter(str.isdigit, x)) for x in _xfam_to(url, pattern)]
 
 def rfam_to_go(family):
+    """get a list of associated GO ids for given rfam access key."""
     url ='https://rfam.xfam.org/family/'+family
     pattern='https://www.ebi.ac.uk/QuickGO/term/'
-    return xfam_to(url, pattern)
+    return _xfam_to(url, pattern)
 
 def rfam_to_so(family):
+    """ get a list of associated SO ids for given rfam access key."""
     url ='https://rfam.xfam.org/family/'+family
     pattern='http://www.sequenceontology.org/miso/'
-    return xfam_to(url, pattern)
+    return _xfam_to(url, pattern)
 
 def rfam_to_ena(family):
+    """get a list of associated ENA ids for given rfam access key."""
     url ='https://rfam.xfam.org/family/'+family+'/sequences'
     pattern='https://www.ebi.ac.uk/ena/data/view/'
-    return xfam_to(url, pattern)
+    return _xfam_to(url, pattern)
 
 def rfam_to_pdb(family):
-        url ='https://rfam.xfam.org/family/'+family+'/structures'
-        pattern='http://www.rcsb.org/pdb/'
-        return [i.upper() for i in xfam_to(url, pattern, sep='=')]
+    """get a list of associated PDB ids for given rfam access key."""
+    url ='https://rfam.xfam.org/family/'+family+'/structures'
+    pattern='http://www.rcsb.org/pdb/'
+    return [i.upper() for i in _xfam_to(url, pattern, sep='=')]
     
 def pfam_to_pdb(family):
+    """get a list of associated PDB ids for given pfam access key."""
     url ='https://pfam.xfam.org/family/'+family+'/mapping'
     pattern='https://www.ebi.ac.uk/interpro/structure/PDB/'
-    return xfam_to(url,pattern)
+    return _xfam_to(url,pattern)
 
 def pfam_to_so(family):
+    """get a list of associated SO ids for given pfam access key."""
     url ='https://pfam.xfam.org/family/'+family
     pattern='http://www.sequenceontology.org/miso/'
-    return xfam_to(url, pattern)
+    return _xfam_to(url, pattern)
 
 def pfam_to_pubmed(family):
+    """get a list of associated pubmed ids for given pfam access key."""
     url='https://pfam.xfam.org/family/'+family
     pattern='http://www.ncbi.nlm.nih.gov/pubmed/'
-    return xfam_to(url,pattern)
+    return _xfam_to(url,pattern)
 
 def pfam_to_go(family):
+    """get a list of associated GO ids for given pfam access key."""
     url ='https://pfam.xfam.org/family/'+family
     pattern='http://www.ebi.ac.uk/ego/'
-    return xfam_to(url, pattern,sep='=')
+    return _xfam_to(url, pattern,sep='=')
