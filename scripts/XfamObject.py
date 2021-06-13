ff8de839df200592a41573f8a1349211f1bdb697
xadam97x <45838015+xadam97x@users.noreply.github.com>
2021-06-13 19:59:19 +0200

    add docs

--- scripts/XfamObject.py
+++ scripts/XfamObject.py
@@ -6,8 +6,17 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 class XfamObject():
+    """An abstract class, base for PfamFamily,PfamClan,RfamFamily and RfamClan."""
     
     def __init__(self,name,db,type_):
+        """constructor method.
+        :param name: rfam/pfam entry access or id
+        :type name: str
+        :param db: database, allowed 'pfam' or 'rfam'
+        :type db: str
+        :param type_: type of entry, allowed 'family' or 'clan'
+        :type type_: str
+        """
         self.db=db
         self.access = self.__set_access(db, type_, name)
         self.short_name = self.__set_id(db, type_, name)
@@ -15,6 +24,14 @@ class XfamObject():
             raise ValueError()
     
     def __set_access(self, db,type_,name):
+        """get access key for a given name
+        :param name: access or id of entry
+        :type name: str
+        :param type_: type of entry - clan or family
+        :type type_: str
+        :return: access key
+        :rettype: str
+        """
         url = 'https://%s.xfam.org/%s/%s' % (db,type_,name)
         r = requests.get(url, allow_redirects=True)
         r.raise_for_status()
@@ -22,6 +39,14 @@ class XfamObject():
         acc = titles[titles.find("(")+1:titles.find(")")]
         return acc
     def __set_id(self, db, type_, name):
+        """get id for a given name
+        :param name: access or id of entry
+        :type name: str
+        :param type_: type of entry - clan or family
+        :type type_: str
+        :return: entry id
+        :rettype: str
+        """
         url = 'https://%s.xfam.org/%s/%s' % (db,type_,name)
         r = requests.get(url, allow_redirects=True)
         r.raise_for_status()
@@ -35,6 +60,10 @@ class XfamObject():
         return rep
     
     def get_tree(self):
+        """Get phylogenetic tree of entry if entry is not a clan.
+        :return: Phylogenetic tree in newick format 
+        :rettype: Bio.Phylo.Newick.Tree
+        """
         if self.type=='clan':
             return None
         url = 'https://%s.xfam.org/family/%s/tree/' % (self.db,self.access)
