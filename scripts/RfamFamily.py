579917fc1bb0523fa87ba8ab6593f79dca7199d3
xadam97x <45838015+xadam97x@users.noreply.github.com>
2021-06-13 19:21:13 +0200

    add docs

--- scripts/RfamFamily.py
+++ scripts/RfamFamily.py
@@ -2,7 +2,27 @@ from XfamObject import *
 from xfam_to import *
 
 class RfamFamily(XfamObject):
+    """contains information about Rfam familly.
+    :param access: Rfam access of family
+    :type access: str
+    :param short_name: Rfam id of family
+    :type short_name: str
+    :param type: family
+    :type type: str
+    :param go_ref: List of GO ids associated with a family
+    :type go_ref: list
+    :param so_ref: List of SO ids associated with a family
+    :type so_ref: list
+    :param pubmed_ref: List of Pubmed ids associated with a family
+    :type pubmed_ref: list
+    :param pdb_ref: List of PDB names associated with a family
+    :type pdb_ref: list
+    """
     def __init__(self,family):
+        """Constructor method.
+        :param family: rfam access or family id
+        :type family: str
+        """
         super().__init__(family,db='rfam',type_='family')
         self.type='family'
         self.go_ref=rfam_to_go(self.access)
@@ -10,7 +30,10 @@ class RfamFamily(XfamObject):
         self.pubmed_ref=rfam_to_pubmed(self.access)
         self.pdb_ref=rfam_to_pdb(self.access)
         
-    def set_sequences(self):
+    def get_sequences(self):
+        """Get seed alignemts of RfamFamily sequences in fasta format.
+        :return: Biopython generator of sequences from alignment  
+        :rtype: generator"""
         url = 'https://rfam.xfam.org/family/%s' % self.access
         url += '/alignment?acc=%s' % self.access
         url += '&format=fastas&download=0'
