ebf2b1791f5539a005d55bf784504c9f2a853ee7
xadam97x <45838015+xadam97x@users.noreply.github.com>
2021-06-13 16:40:29 +0200

    Update PfamFamily.py

--- scripts/PfamFamily.py
+++ scripts/PfamFamily.py
@@ -1,58 +1,67 @@
 from xfam_to import *
+from XfamObject import *
+from xfam_to import *
+from get_architectures import *
 
-class PfamFamily(object):
+class PfamFamily(XfamObject):
     """class contains information about Pfam familly.
     TO DO
     """
-    
-    def __init__(self, acc=None, name=None):
-        self.access=acc
-        self.short_name=name
-        if self.access is None and self.short_name is None:
-            raise ValueError()
-        elif self.access is None:
-            pass #get_alternative()
-        elif self.short_name is None:
-            pass #get_alternative()
-        
-        if re.match('[0-9]',self.short_name[0]):
-            p='numbers'
+    def __init__(self, family):
+        super().__init__(family,db='pfam', type_='family')
+        if re.match('[0-9]', self.short_name[0]):
+            self.p = 'numbers'
         else:
-            p=self.short_name[0].lower()
-            
-        url='http://pfam.xfam.org/family/browse?browse='+p
-        
+            self.p = self.short_name[0].lower()
         try:
-            html=urlopen(url)
+            url = 'http://pfam.xfam.org/family/browse?browse=' + self.p
+            html = urlopen(url)
         except (HTTPError, URLError) as e:
             print(e, url)
             return None
         else:
-            soup=BeautifulSoup(html, 'html.parser')
-            table=soup.find('table',attrs={"class": "details browse"})
-            for a in table.find_all('a',href=True):
-                if a.string==self.short_name:
-                    tr=a.parent.parent
-                    columns=tr.find_all('td')
-                    self.type=columns[2].string
-                    self.seed_len=int(columns[3].string)
-                    self.full_len=int(columns[4].string)
-                    self.avarage_len=float(columns[5].string)
-                    self.avarage_id=float(columns[6].string)
-                    self.avarage_coverage=float(columns[7].string)
-                    self.changestatus=columns[9].string
-                    self.Description=columns[10].string
-        self.go_ref=pfam_to_go(self.access)
-        self.so_ref=pfam_to_so(self.access)
-        self.pubmed_ref=pfam_to_pubmed(self.access)
-        self.pdb_ref=pfam_to_pdb(self.access)
-        
-    def __str__(self):
-        rep='Pfam '+self.type+' '+self.short_name+' '
-        rep+='; Pfam Access: '
-        rep+=self.access
-        return rep
+            soup = BeautifulSoup(html, 'html.parser')
+            table = soup.find('table', attrs={"class": "details browse"})
+            for a in table.find_all('a', href=True):
+                if a.string == self.short_name:
+                    tr = a.parent.parent
+                    columns = tr.find_all('td')
+                    self.type = columns[2].string
+                    self.seed_len = int(columns[3].string)
+                    self.full_len = int(columns[4].string)
+                    self.avarage_len = float(columns[5].string)
+                    self.avarage_id = float(columns[6].string)
+                    self.avarage_coverage = float(columns[7].string)
+                    self.changestatus = columns[9].string
+                    self.Description = columns[10].string
+                    self.tree = self.__set_tree()
+                    self.seed = self.__set_seed()
+                    self.full = self.__set_full()
+        self.go_ref = pfam_to_go(self.access)
+        self.so_ref = pfam_to_so(self.access)
+        self.pubmed_ref = pfam_to_pubmed(self.access)
+        self.pdb_ref = pfam_to_pdb(self.access)
+
+
+    def __set_full(self):
+        url = 'https://pfam.xfam.org/family/%s' % self.access
+        url += '/alignment/full'
+        url += '/format?format=fasta&alnType=fasta&order=a&case=l&gaps=dashes&download=1'
+        r = requests.get(url, allow_redirects=True)
+        r.raise_for_status()
+        full  = SeqIO.parse(StringIO(r.text), 'fasta')
+        return full
+
+
+    def __set_seed(self):
+        url = 'https://pfam.xfam.org/family/%s' % self.access
+        url += '/alignment/seed'
+        url += '/format?format=fasta&alnType=fasta&order=a&case=l&gaps=dashes&download=1'
+        r = requests.get(url, allow_redirects=True)
+        r.raise_for_status()
+        seed  = SeqIO.parse(StringIO(r.text), 'fasta')
+        return seed
     
-    # get sequences
-    # get tree
-    # get hmm
+    def get_architectures(self):
+        return family_architectures(self.access)
+
