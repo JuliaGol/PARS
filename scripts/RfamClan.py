ec6d35309071ef2ee247d47b3ab240d05a3e8ad1
xadam97x <45838015+xadam97x@users.noreply.github.com>
2021-06-13 23:27:30 +0200

    add __set_members method

--- scripts/RfamClan.py
+++ scripts/RfamClan.py
@@ -11,6 +11,8 @@ class RfamClan(XfamObject):
     :type type: str
     :param pdb-ref: List of PDB names associated with a family
     :type pdb_ref: list
+    :param members: List of families that are members of this clan
+    :type members: list
     """
     def __init__(self,clan):
         """Constructor method.
@@ -20,3 +22,17 @@ class RfamClan(XfamObject):
         super().__init__(clan,db='rfam',type_='clan')
         self.type='clan'
         self.pdb_ref=rfam_clan_to_pdb(self.access)
+        self.members=self.__set_members()
+        
+    def __set_members(self):
+        """Method for setting members attribute.
+        
+        :return: List of clan members
+        :rettype: list"""
+        
+        clan=self.access
+        clan_url = 'https://rfam.xfam.org/clan/%s#tabview=tab2' % clan
+        r_clan = requests.get(clan_url, allow_redirects=True)
+        r_clan.raise_for_status()
+        families = re.findall('RF[0-9]{5}',r_clan.text)
+        return list(set(families))
