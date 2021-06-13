fe0f612cbd728eb240db9a4d80df31649a71ee08
xadam97x <45838015+xadam97x@users.noreply.github.com>
2021-06-13 20:09:08 +0200

    edit docs

--- scripts/xfam_to.py
+++ scripts/xfam_to.py
@@ -20,66 +20,104 @@ def _xfam_to(url, pattern, sep='/'):
 
         
 def rfam_to_pubmed(family):
-    """get a list of associated pubmed ids for given rfam access key."""
+    """get a list of associated pubmed ids for given rfam access key.
+    :param family: rfam accession key of family
+    :type family: str
+    :return: List of associated pubmed ids
+    :rettype:list
+    """
     url ='https://rfam.xfam.org/family/'+family
     pattern='http://www.ncbi.nlm.nih.gov/pubmed/'
     return ["".join(filter(str.isdigit, x)) for x in _xfam_to(url, pattern)]
 
 def rfam_to_go(family):
-    """get a list of associated GO ids for given rfam access key."""
+    
+    """get a list of associated GO ids for given rfam access key.
+    :param family: rfam accession key of family
+    :type family: str
+    :return: List of associated Go ids
+    :rettype:list"""
     url ='https://rfam.xfam.org/family/'+family
     pattern='https://www.ebi.ac.uk/QuickGO/term/'
     return _xfam_to(url, pattern)
 
 def rfam_to_so(family):
-    """ get a list of associated SO ids for given rfam access key."""
+    """ get a list of associated SO ids for given rfam access key.
+    :param family: rfam accession key of family
+    :type family: str
+    :return: List of associated SO ids
+    :rettype:list"""
     url ='https://rfam.xfam.org/family/'+family
     pattern='http://www.sequenceontology.org/miso/'
     return _xfam_to(url, pattern)
 
-def rfam_to_ena(family):
-    """get a list of associated ENA ids for given rfam access key."""
-    url ='https://rfam.xfam.org/family/'+family+'/sequences'
-    pattern='https://www.ebi.ac.uk/ena/data/view/'
-    return _xfam_to(url, pattern)
 
 def rfam_to_pdb(family):
-    """get a list of associated PDB ids for given rfam access key."""
+    """get a list of associated PDB ids for given rfam access key.
+    :param family: rfam accession key of family
+    :type family: str
+    :return: List of associated PDB ids
+    :rettype:list"""
     url ='https://rfam.xfam.org/family/'+family+'/structures'
     pattern='http://www.rcsb.org/pdb/'
     return [i.upper() for i in _xfam_to(url, pattern, sep='=')]
     
 def pfam_to_pdb(family):
-    """get a list of associated PDB ids for given pfam access key."""
+    """get a list of associated PDB ids for given pfam access key.
+    :param family: pfam accession key of family
+    :type family: str
+    :return: List of associated PDB ids
+    :rettype:list"""
     url ='https://pfam.xfam.org/family/'+family+'/mapping'
     pattern='https://www.ebi.ac.uk/interpro/structure/PDB/'
     return _xfam_to(url,pattern)
 
 def pfam_to_so(family):
-    """get a list of associated SO ids for given pfam access key."""
+    """get a list of associated SO ids for given pfam access key.
+    :param family: pfam accession key of family
+    :type family: str
+    :return: List of associated SO ids
+    :rettype:list"""
     url ='https://pfam.xfam.org/family/'+family
     pattern='http://www.sequenceontology.org/miso/'
     return _xfam_to(url, pattern)
 
 def pfam_to_pubmed(family):
-    """get a list of associated pubmed ids for given pfam access key."""
+    """get a list of associated pubmed ids for given pfam access key.
+    :param family: pfam accession key of family
+    :type family: str
+    :return: List of associated Pubmed ids
+    :rettype:list"""
     url='https://pfam.xfam.org/family/'+family
     pattern='http://www.ncbi.nlm.nih.gov/pubmed/'
     return _xfam_to(url,pattern)
 
 def pfam_to_go(family):
-    """get a list of associated GO ids for given pfam access key."""
+    """get a list of associated GO ids for given pfam access key.
+    :param family: pfam accession key of family
+    :type family: str
+    :return: List of associated GO ids
+    :rettype:list"""
     url ='https://pfam.xfam.org/family/'+family
     pattern='http://www.ebi.ac.uk/ego/'
     return _xfam_to(url, pattern,sep='=')
 
 def pfam_clan_to_pdb(clan):
+    """get a list of associated PDB ids for given pfam clan access key.
+    :param clan: pfam accession key of clan
+    :type clan: str
+    :return: List of associated PDB ids
+    :rettype:list"""
     url='http://pfam.xfam.org/clan/'+clan+'/structures'
     pattern='/structure/[A-Z, 0-9]{4}'
     return _xfam_to(url,pattern)
 
 def rfam_clan_to_pdb(clan):
-    """get a list of associated PDB ids for given rfam access key."""
+    """get a list of associated PDB ids for given rfam clan access key.
+    :param clan: rfam accession key of clan
+    :type clan: str
+    :return: List of associated PDB ids
+    :rettype:list"""
     url ='https://rfam.xfam.org/clan/'+clan+'/structures'
     pattern='http://www.rcsb.org/pdb/'
     return [i.upper() for i in _xfam_to(url, pattern, sep='=')]
