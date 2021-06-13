52db4508c66410187dcba5f92d7007a0883d4b00
xadam97x <45838015+xadam97x@users.noreply.github.com>
2021-06-13 19:41:15 +0200

    add docs

--- scripts/PfamArchitecture.py
+++ scripts/PfamArchitecture.py
@@ -1,4 +1,10 @@
 class PfamArchitecture():
+    """Simple class for protein domains architecture.
+    :param domains: list of domains in the architecture
+    :type domains: list
+    :param number_of_sequences: number of sequences with this architecture
+    :type number_of_sequences: int
+    """
     def __init__(self,text):
         self.domains=text.split(':')[-1].split(', ')
         self.domains=[domain.replace(' ','') for domain in self.domains]
