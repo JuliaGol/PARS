cd601221ae8404612ed6074bcce63c0cd67623ac
xadam97x <45838015+xadam97x@users.noreply.github.com>
2021-06-14 15:44:59 +0200

    Create __init__.py

---
+++ pars/__init__.py
@@ -0,0 +1,17 @@
+#__init__py
+
+from .family_architectures import *
+from .hammer_to_object import*
+from .hmm_autosearch import*
+from .hmm_download import*
+from .hmmer_command import*
+from .hmmer_file import*
+from .pfam_architecture import*
+from .pfamClan import*
+from .pfam_family import*
+from .pfam_sequences import*
+from .rfam_clan import*
+from .rfam_family import*
+from .rfamily_seq import*
+from .xfam_object import*
+from .xfam_to import*
